e9954f8efe6753635ef989741a338a1efeb26902
Jeremy Axmacher <jeremy@obsoleter.com>
2019-11-14 02:08:31 -0500

    Update run script to start server

--- run.sh
+++ run.sh
@@ -1,9 +1,27 @@
-#!/bin/bash
+#!/bin/bash -xe
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-pip install -e .
+yum update -y
+yum install -y shadow-utils util-linux git python3 mariadb-devel gcc python3-devel
+adduser web
 
-export APP_NAME=example-1
-export ENV_NAME=prod
-export AWS_DEFAULT_REGION=us-east-1
+cat <<EOF >/tmp/run.sh
+git clone https://github.com/jcaxmacher/microblog.git;
+python3 -m venv venv;
+source venv/bin/activate;
+cd microblog;
+pip install -e .;
+
+export FLASK_APP=microblog
+export APP_NAME=example-a;
+export ENV_NAME=prod;
+export AWS_DEFAULT_REGION=us-east-1;
+sleep 120
+flask init-db
 
 gunicorn -w 4 microblog.wsgi:app -b 0.0.0.0:5000
+EOF
+
+# pyagent run -c /path/to/appdynamics.cfg -- gunicorn -w 8 -b '0.0.0.0:9000' appdynamics.scripts.wsgi:application
+
+sudo -i -H -u web bash -c "$(cat /tmp/run.sh)"
